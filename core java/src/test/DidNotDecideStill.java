782d83fc7d6cf3fa354e220dd3e9536887f10b39
vamsi garikipati <vamsigarikipati@yahoo.com>
2016-04-09 21:13:16 -0500

    Signed-off-by: vamsi garikipati <vamsigarikipati@yahoo.com>

--- core java/src/test/DidNotDecideStill.java
+++ core java/src/test/DidNotDecideStill.java
@@ -2,9 +2,34 @@ package test;
 
 public class DidNotDecideStill {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int t;
+	private String n;
 
+	public int getT() {
+		return t;
+	}
+
+	public void setT(int t) {
+		this.t = t;
+	}
+
+	public String getN() {
+		return n;
+	}
+
+	public void setN(String n) {
+		this.n = n;
+	}
+
+	public static void main(String[] args) {
+		DidNotDecideStill d = new DidNotDecideStill(7, "vamsi");
+		System.out.println(d.getN());
+	}
+
+	public DidNotDecideStill(int t, String n) {
+		super();
+		this.t = t;
+		this.n = n;
 	}
 
 }
